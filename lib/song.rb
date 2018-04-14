bc1ef92c483cc161ab2d6544745f03b6e06e83b2
Ally Netzel <netzelally@gmail.com>
2018-04-14 13:41:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,7 @@ class Song
   def initialize(name)
     @name = name 
     @file_name = file_name
-    puts self.name 
-end
+  end
 
   def self.new_by_filename(filename)
     song = self.new(filename)
