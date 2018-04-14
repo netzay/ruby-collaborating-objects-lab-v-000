ebeefb39e15054413b36912bfbc045cef8bdfd84
Ally Netzel <netzelally@gmail.com>
2018-04-14 13:43:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename)
-    song.name = filename.split(" - ")[1]
+    song_name = filename.split(" - ")[1]
   end
 
   def artist_name=(name)
